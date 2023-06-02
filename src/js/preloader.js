d69446fa7136e3c2deae57cba095bf100cfc3629
Riosahid25 <riosahid25@gmail.com>
2023-06-02 11:07:29 +0700

    menyelesaikan asset pada intro dan page danau toba

--- src/js/preloader.js
+++
@@ -1,9 +0,0 @@
-document.body.onload = function () {
-  /*The onload event occurs when an object has been loaded*/
-  setTimeout(function () {
-    var preloader = document.getElementById("page-preloader");
-    {
-      preloader.classList.add("done");
-    }
-  }, 800);
-};
